c910a36030dc6534578a681c20ba153b0cd23adf
Julius Künzel <jk.kdedev@smartlab.uber.space>
2021-10-14 16:13:14 +0200

    Add HTTP 404 handler (borrowed from krita docs)

--- source/conf.py
+++ source/conf.py
@@ -41,6 +41,8 @@ templates_path = ['../ressources/templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
+html_extra_path = ['404handler.php'] # bring our 404 handler in
+
 
 # The master toctree document.
 master_doc = 'index'
