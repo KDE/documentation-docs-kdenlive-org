6fb152cacaaec0cb0c110ee8e0ce91b70ef53edd
Julius Künzel <jk.kdedev@smartlab.uber.space>
2021-11-16 11:32:15 +0100

    Make some very long filenames shorter

--- tests/check_filenames.py
+++ tests/check_filenames.py
@@ -38,6 +38,23 @@ class TestFilenames(unittest.TestCase):
 
                 pass
 
+    def test_filenameLength(self):
+        # Filenames longer than 150 chars can't be handled by Scripty's SQL Database
+        for dirpath, dirnames, filenames in os.walk(SRC_DIR):
+            for f in filenames:
+
+                # Filter out files
+                if not f.endswith('.rst'):
+                    continue
+
+                pot = 'docs_kdenlive_org_'
+                pot = pot + dirpath.replace('./','').replace('/','__')
+                pot = pot + '__' + f.replace('.rst', '.pot')
+
+                if(len(pot) > 150):
+                    print('Filename ' + pot + ' to long for Scripty!')
+                self.assertFalse(len(pot) > 150)
+
 
 if __name__ == "__main__":
     unittest.main()
